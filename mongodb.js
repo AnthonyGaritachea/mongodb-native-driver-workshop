4be4bf23749ca6643cad0d1a1f9d7c254b1a3cda
Anthony Garitachea <lgaritachea45204@gmail.com>
2019-11-21 13:50:16 -0800

    adds find queries

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,4 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const database = 'task-manager';
@@ -9,7 +8,8 @@ MongoClient.connect(connectionUrl, {useNewUrlParser: true}, (error, client) => {
         return console.log('unable to connect to database')
     }
    const db = client.db(database)
-
+                     // CREATE
+                     
 //    db.collection('users').insertOne({
 //        name: 'Anthony',
 //        age: '22'
@@ -24,23 +24,65 @@ MongoClient.connect(connectionUrl, {useNewUrlParser: true}, (error, client) => {
     //         age: 27
     //     }
     // ])
-    db.collection('tasks').insertMany([
-        {
-            description: 'finish projects',
-            completed: false
-        },
-        {
-            description: 'change engine oil',
-            completed: true
-        },
-        {
-            description: 'shop for groceries',
-            completed: false
-        }], (error, result) => {
-            if (error) {
-                return console.log(error)
-            }
-            console.log(result.ops)
-        } 
-    )
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'finish projects',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'change engine oil',
+    //         completed: true
+    //     },
+    //     {
+    //         description: 'shop for groceries',
+    //         completed: false
+    //     }], (error, result) => {
+    //         if (error) {
+    //             return console.log(error)
+    //         }
+    //         console.log(result.ops)
+    //     } 
+    // )
+
+                           // READ
+
+    // db.collection('users').findOne({}, (error, user) => {
+    //     if (error){
+    //        return console.log(error) 
+    //     }
+    //     console.log(user)
+    // });
+
+    // db.collection('users').find({}, (error, user) => {
+    //     if (error){
+    //        return console.log(error) 
+    //     }
+    //     console.log(user)
+    // });
+                
+    // db.collection('users').findOne({_id : new ObjectID("5dd5e1b441e1b11db23c5210")}, (error, user) => {
+    //     if (error){
+    //        return console.log(error) 
+    //     }
+    //     console.log(user)
+    // });
+
+    // db.collection('tasks').findOne({_id: new ObjectID('5dd5eb5e2884117efbef15c6')}, (error, task) => {
+    //     if(error){
+    //         return console.log(error)
+    //     }
+    //     console.log(task)
+    // });
+            
+    // db.collection('users').find({name: "Gunther"}).toArray((error, users) => {
+    //     console.log(users)
+    // });
+    
+    // db.collection('tasks').find({completed: false}).toArray((error, task) => {
+    //     console.log(task)
+    // });
+
+    db.collection('users').find({name: "Gunther"}).count((error, count) => {
+        console.log(count)
+    });
 });
